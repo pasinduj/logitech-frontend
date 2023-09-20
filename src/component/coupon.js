3dcaa822bb4eaab3972ceb81e9a4adb1be25dd00
pasinduj <pasinduk@zuse.lk>
2023-09-21 03:53:18 +0800

    third commit

---
+++ src/component/coupon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './coupon.css'
+
+const Coupon = () => {
+   
+    return (
+        <>
+            <div className="coupon">MI50</div>
+        </>
+    )
+}
+
+export default Coupon
